dc0a2ffbd3e94f084deda20f5bdf02997bd32726
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-30 19:42:58 -0400

    Added location spots

--- assets/js/aquariums.js
+++ assets/js/aquariums.js
@@ -1,10 +1,3 @@
-//Variables
-// const apiKey = "Vsn/suc5VIeT+CF4MGJtlA==Jo3z5DiNaWaU6u2H";
-// const animal = "";
-// const searchInput = "";
-// const animalsList = [];
-// const searchButton = $("#.searchButton");
-
 //Function that allows the user to search for a specific animal
 async function searchAnimals() {
   var searchInput = document.querySelector(".input").value;
@@ -14,21 +7,11 @@ async function searchAnimals() {
     mode: "cors",
     headers: {
       "X-RapidAPI-Key": "43e968232emsh327e4df849cd7f3p1f85dajsn3ad7209370f7",
-      // "X-RapidAPI-Host": "animals-by-api-ninjas.p.rapidapi.com",
       "Content-Type": "application/json",
     },
   };
 
-  // const apiUrl = `https://api.api-ninjas.com/v1/animals?name=${searchInput}`;
-
-  // const options = {
-  //   method: "GET",
-  //   mode: "cors",
-  //   headers: {
-  //     "X-Api-Key": apiKey,
-  //   },
-  // };
-
+  //
   fetch(apiUrl, options)
     .then((response) => response.json())
     .then((data) => {
@@ -48,8 +31,7 @@ async function searchAnimals() {
       characteristicElement.innerHTML = `Color: ${animal.characteristics.color}`;
       informationElement.appendChild(characteristicElement);
 
-      //
-
+      //ADD THE REST OF THESE TO ABOVE CODE
       animal.characteristics.common_name;
       animal.characteristics.diet;
       animal.characteristics.estimated_population_size;
@@ -62,50 +44,13 @@ async function searchAnimals() {
       animal.characteristics.prey;
       animal.characteristics.top_speed;
       animal.characteristics.weight;
+
+      var locationElement = document.createElement("div");
+      locationElement.innerHTML = `Locations: ${animal.locations.join(", ")}`;
+      informationElement.appendChild(locationElement);
     });
 }
 
 //Event Listener [Do I want to put this in the above function]
 const searchButton = document.querySelector(".search-button button");
 searchButton.addEventListener("click", searchAnimals);
-
-// Event listener for user search input
-// const searchButton = document.querySelector(".search-button button");
-// searchButton.addEventListener("click", searchAnimal);
-
-// User input that will call the API to return
-// information about a particualar animal that the user inputs
-// async function searchAnimal() {
-//   const input = document.querySelector(".searchbox input");
-//   const animalName = input.value;
-//   const url = `https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=${animalName}`;
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       "X-RapidAPI-Key": "43e968232emsh327e4df849cd7f3p1f85dajsn3ad7209370f7",
-//       "X-RapidAPI-Host": "animals-by-api-ninjas.p.rapidapi.com",
-//     },
-//   };
-
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.json();
-//     displayAnimal(result[0]);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// Function that allows the animal information
-// to be rendered on the application
-function displayAnimal(animal) {
-  const name = document.querySelector(".title");
-  const scientificName = document.querySelector(".subtitle");
-  const image = document.querySelector("img");
-  const information = document.querySelector(".information");
-
-  name.textContent = animal.name;
-  scientificName.textContent = animal.scientific_name;
-  image.src = animal.image;
-  information.textContent = animal.description;
-}
