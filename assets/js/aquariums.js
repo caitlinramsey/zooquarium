234f8d474788e90e726f77284adb6b020937f9fa
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-30 18:17:43 -0400

    Got a 200 msg from api call

--- assets/js/aquariums.js
+++ assets/js/aquariums.js
@@ -1,82 +1,81 @@
 //Variables
 // const apiKey = "Vsn/suc5VIeT+CF4MGJtlA==Jo3z5DiNaWaU6u2H";
-//Need to figure out how to get it where people can search for any animal
-// const animal = "";
+const animal = "";
 // const searchInput = "";
-// const animalsList = [];
+const animalsList = [];
 // const searchButton = $("#.searchButton");
 
 //Function that allows the user to search for a specific animal
-// async function searchAnimals() {
-//Need to rename searchInput with HTML element
-// document.getElementById("searchInput").value;
-// const apiKey = `https://api-ninjas.com/api/animals?search=${searchInput}`;
-
-// fetch(apiKey)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const animalsList = document.getElementById("animalsList");
-//     animalsList.innerHTML = "";
-
-//     data.forEach((animal) => {
-//       const animalElement = document.createElement("div");
-//       animalElement.innerHTML = `<h2>${animal.name}</h2><img src="${animal.image}" alt="${animal.name}">`;
-//       animalsList.appendChild(animalElement);
-//     });
-//   });
-// }
-
-// const url = 'https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=cheetah';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '43e968232emsh327e4df849cd7f3p1f85dajsn3ad7209370f7',
-// 		'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-
-//Event Listener [Do I want to put this in the above function]
-// const searchButton = document.querySelector(".search-button button");
-// searchButton.addEventListener("click", searchAnimals);
-
-//Event listener for user search input
-const searchButton = document.querySelector(".search-button button");
-searchButton.addEventListener("click", searchAnimal);
-
-//User input that will call the API to return
-//information about a particualar animal that the user inputs
-async function searchAnimal() {
-  const input = document.querySelector(".searchbox input");
-  const animalName = input.value;
-  const url = `https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=${animalName}`;
+async function searchAnimals() {
+  var searchInput = document.querySelector(".input").value;
+  const apiUrl = `https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=${searchInput}`;
   const options = {
     method: "GET",
+    mode: "cors",
     headers: {
       "X-RapidAPI-Key": "43e968232emsh327e4df849cd7f3p1f85dajsn3ad7209370f7",
-      "X-RapidAPI-Host": "animals-by-api-ninjas.p.rapidapi.com",
+      // "X-RapidAPI-Host": "animals-by-api-ninjas.p.rapidapi.com",
+      "Content-Type": "application/json",
     },
   };
 
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    displayAnimal(result[0]);
-  } catch (error) {
-    console.error(error);
-  }
+  // const apiUrl = `https://api.api-ninjas.com/v1/animals?name=${searchInput}`;
+
+  // const options = {
+  //   method: "GET",
+  //   mode: "cors",
+  //   headers: {
+  //     "X-Api-Key": apiKey,
+  //   },
+  // };
+
+  fetch(apiUrl, options)
+    .then((response) => response.json())
+    .then((data) => {
+      const animalsList = document.getElementById("animalsList");
+      animalsList.innerHTML = "";
+
+      data.forEach((animal) => {
+        const animalElement = document.createElement("div");
+        animalElement.innerHTML = `<h2>${animal.name}</h2><img src="${animal.image}" alt="${animal.name}">`;
+        animalsList.appendChild(animalElement);
+      });
+    });
 }
 
+//Event Listener [Do I want to put this in the above function]
+const searchButton = document.querySelector(".search-button button");
+searchButton.addEventListener("click", searchAnimals);
 
-//Function that allows the animal information
-//to be rendered on the application
+// Event listener for user search input
+// const searchButton = document.querySelector(".search-button button");
+// searchButton.addEventListener("click", searchAnimal);
+
+// User input that will call the API to return
+// information about a particualar animal that the user inputs
+// async function searchAnimal() {
+//   const input = document.querySelector(".searchbox input");
+//   const animalName = input.value;
+//   const url = `https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=${animalName}`;
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "43e968232emsh327e4df849cd7f3p1f85dajsn3ad7209370f7",
+//       "X-RapidAPI-Host": "animals-by-api-ninjas.p.rapidapi.com",
+//     },
+//   };
+
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     displayAnimal(result[0]);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// Function that allows the animal information
+// to be rendered on the application
 function displayAnimal(animal) {
   const name = document.querySelector(".title");
   const scientificName = document.querySelector(".subtitle");
