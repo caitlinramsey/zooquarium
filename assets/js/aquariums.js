2f13403781443e04e3dbfe6d969b91295a8df3c5
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-30 15:01:54 -0400

    added id to html to allow search to work

--- assets/js/aquariums.js
+++ assets/js/aquariums.js
@@ -2,14 +2,14 @@
 const apiKey = "Vsn/suc5VIeT+CF4MGJtlA==Jo3z5DiNaWaU6u2H";
 //Need to figure out how to get it where people can search for any animal
 const animal = "";
-const searchInput = "";
-const animalsList = [];
+// const searchInput = "";
+// const animalsList = [];
 // const searchButton = $("#.searchButton");
 
 //Function that allows the user to search for a specific animal
 async function searchAnimals() {
   //Need to rename searchInput with HTML element
-  const searchInput = document.getElementById("searchButton").value;
+  document.getElementById("searchInput").value;
   const apiKey = `https://api-ninjas.com/api/animals?search=${searchInput}`;
 
   fetch(apiKey)
